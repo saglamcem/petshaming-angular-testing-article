57aec57e480ef38600d7de77247ff66d3f94961d
Cem Saglam <cemdevp@gmail.com>
2021-01-31 16:55:05 +0300

    feat: create material module

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const materialModules = [
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...materialModules
+  ],
+  exports: [
+    ...materialModules
+  ]
+})
+export class MaterialModule { }
