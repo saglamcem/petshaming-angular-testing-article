e9b81857dfbc7c0b36a81a0f94ca21af21538ec9
Cem Saglam <cemdevp@gmail.com>
2021-02-03 11:21:44 +0300

    feat!: sort posts descendingly by number of likes
    - contains fdescribe in sort-posts.spec.ts
    - also removes .gitkeep

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { Observable, of } from 'rxjs';
 import { PetPost } from '../models/post.model';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { sortPostsByLikesDesc } from '../utils/sort-posts';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,13 @@ export class PostService {
     return this.dataService.get(path)
       .pipe(
         catchError(err => of([]))
+      );
+  }
+
+  getPetPostsSortedByLikes(path: string): Observable<PetPost[]> {
+    return this.getPetPosts(path)
+      .pipe(
+        map(posts => sortPostsByLikesDesc(posts))
       );
   }
 }
