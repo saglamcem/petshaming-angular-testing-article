ba46537d0886dcc3b6a705542802c97719d71e2c
Cem Saglam <cemdevp@gmail.com>
2021-02-03 11:10:21 +0300

    feat: prepare folder structure for the following article

---
+++ src/app/__mocks__/sort-posts.mock.ts
@@ -0,0 +1,77 @@
+import { PetPost } from '../models/post.model';
+
+export const unsortedListOfPosts: PetPost[] = [
+  {
+    name: 'Homer',
+    quote: `I tore open the cat nip and now I'm too stoned to move ...`,
+    numberOfLikes: 2374,
+    imageUrl: `https://static.boredpanda.com/blog/wp-content/uploads/2015/03/cat-shaming-16__605.jpg`,
+    isLiked: true
+  },
+  {
+    name: 'Bart',
+    quote: 'I watched a mouse eat my food and did nothing',
+    numberOfLikes: 23,
+    imageUrl: 'https://static.boredpanda.com/blog/wp-content/uploads/2015/03/cat-shaming-3__605.jpg',
+    isLiked: false
+  },
+  {
+    name: 'Abed',
+    quote: `I poop at the bottom of the kids' slide.`,
+    numberOfLikes: 374,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/Screen-Shot-2013-10-02-at-1.41.48-PM.png',
+    isLiked: true
+  },
+  {
+    name: 'Troy',
+    quote: 'Peed on Yoda I did',
+    numberOfLikes: 0,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/Screen-Shot-2013-10-02-at-1.18.23-PM.png',
+    isLiked: false
+  },
+  {
+    name: 'Steve',
+    quote: 'I stole a baby doll from a six-year-old and ripped its head off!',
+    numberOfLikes: 1,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/dog-shaming-11.jpg',
+    isLiked: true
+  }
+];
+
+export const sortedListOfPosts: PetPost[] = [
+  {
+    name: 'Homer',
+    quote: `I tore open the cat nip and now I'm too stoned to move ...`,
+    numberOfLikes: 2374,
+    imageUrl: `https://static.boredpanda.com/blog/wp-content/uploads/2015/03/cat-shaming-16__605.jpg`,
+    isLiked: true
+  },
+  {
+    name: 'Abed',
+    quote: `I poop at the bottom of the kids' slide.`,
+    numberOfLikes: 374,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/Screen-Shot-2013-10-02-at-1.41.48-PM.png',
+    isLiked: true
+  },
+  {
+    name: 'Bart',
+    quote: 'I watched a mouse eat my food and did nothing',
+    numberOfLikes: 23,
+    imageUrl: 'https://static.boredpanda.com/blog/wp-content/uploads/2015/03/cat-shaming-3__605.jpg',
+    isLiked: false
+  },
+  {
+    name: 'Steve',
+    quote: 'I stole a baby doll from a six-year-old and ripped its head off!',
+    numberOfLikes: 1,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/dog-shaming-11.jpg',
+    isLiked: true
+  },
+  {
+    name: 'Troy',
+    quote: 'Peed on Yoda I did',
+    numberOfLikes: 0,
+    imageUrl: 'https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/10/Screen-Shot-2013-10-02-at-1.18.23-PM.png',
+    isLiked: false
+  },
+];
